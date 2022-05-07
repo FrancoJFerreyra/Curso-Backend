d3775ee1d1913c04f76dbf17e8f162b0cfe303af
Franco Ferreyra <ferreyra_franco02@outlook.com>
2022-05-07 16:03:44 -0300

    Heroku con Express

--- express.js
+++ express.js
@@ -44,7 +44,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(cookieParser());
 app.use(
-  
   session({
     store: mongoStore.create({
       mongoUrl:
